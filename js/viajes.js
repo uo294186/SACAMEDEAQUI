0cff05a9f80febb9040d9ad467daa70bb5e262cc
uo294186 <158302777+uo294186@users.noreply.github.com>
2024-12-08 19:40:19 +0100

    Add files via upload

--- js/viajes.js
+++ js/viajes.js
@@ -38,16 +38,12 @@ class Viajes{
 
     }
 
-    async showStaticMap(){
-        var p = $("<p></p>");
-        
-        p.text("Mapa estático");
-
+    showStaticMap(){
+    
         var apiKey = "&key=AIzaSyC6j4mF6blrc4kZ54S6vYZ2_FpMY9VzyRU";
         var url = "https://maps.googleapis.com/maps/api/staticmap?";
         var centro = "center="+this.latitud+","+this.longitud;
-
-        var tamaño = "&size=200x100";
+        var tamaño = "&size=800x600";
         var marcador = "&markers=color:red%7Clabel:P%7C"+this.latitud+","+this.longitud;
 
         var mapaImagen = url+centro+tamaño+marcador+apiKey;
@@ -57,7 +53,7 @@ class Viajes{
             alt : "mapa estático de tu localización"
         });
 
-        $("main > section.first").append(p);
+        
         $("main > section:first").append(img);
         
 
@@ -65,13 +61,10 @@ class Viajes{
         
     }
 
-    async showDynamicMap(){
+    showDynamicMap(){
         var centro = {lat: 0, lng:0};
 
-        const { Map } = await google.maps.importLibrary("maps");
-
-
-        var mapa = new Map(document.querySelector("main div"), {
+        var mapa = new google.maps.Map(document.querySelector("main div"), {
             zoom : 15,
             center: centro,
             mapTypeId: google.maps.MapTypeId.ROADMAP
